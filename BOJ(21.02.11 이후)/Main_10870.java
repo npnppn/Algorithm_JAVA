3b40edba8c69680709804359d01a9fe8aee54e8c
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-02-12 19:56:02 +0900

    #피보나치 수 5 #재귀 #Bronze2

---
+++ BOJ(21.02.11 이후)/Main_10870.java
@@ -0,0 +1,26 @@
+package ssafy_algo;
+import java.util.*;
+import java.io.*;
+
+public class Main_10870 {
+
+	public static void main(String[] args) throws Exception, IOException {
+
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(recursive(N));
+	}
+	
+	public static int recursive(int num) {
+		
+		if (num == 0) {
+			return 0;
+		}
+		else if (num == 1) {
+			return 1;
+		}
+		else return recursive(num-1) + recursive(num-2);
+	}
+
+}
