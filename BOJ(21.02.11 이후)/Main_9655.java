9717c5167bc7dbddf5257df97307fcf644f667cd
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-02-26 02:30:07 +0900

    #돌 게임 #게임구현 #Silver5

---
+++ BOJ(21.02.11 이후)/Main_9655.java
@@ -0,0 +1,17 @@
+package ssafy_algo;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_9655 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		if (N%2 == 1) {
+			System.out.println("SK");
+		}
+		else System.out.println("CY");
+	}
+
+}
