ef56ebdccbb7f8f912b05d020a86519361d66be8
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-03-17 18:51:29 +0900

    Create Main_2605.java

---
+++ IM대비/Main_2605.java
@@ -0,0 +1,22 @@
+package Im_ex;
+import java.io.*;
+import java.util.*;
+
+public class Main_2605 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		ArrayList<Integer> List = new ArrayList<>();
+		List.add(0);
+		for(int i=1; i<=n; i++) {
+			int num = sc.nextInt();
+			List.add(i-num,i);
+		}
+		for (int i=1; i<=n; i++) {
+			System.out.print(List.get(i) + " ");
+		}
+
+	}
+
+}
