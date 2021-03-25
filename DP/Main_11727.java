9e598e781319d35f14f2535ed31510730be5ca27
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-03-25 11:23:06 +0900

    #2×n 타일링2 #실버3

---
+++ DP/Main_11727.java
@@ -0,0 +1,24 @@
+package ssafy_algo;
+import java.io.*;
+import java.util.*;
+
+public class Main_11727 {
+	static int N;
+	static int[] dx;
+	
+	public static void main(String[] args) throws Exception, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		dx = new int[1001];
+		System.out.println(dp(N));
+	}
+	
+	static int dp(int x) {
+		
+		if(x==1) return 1;
+		if(x==2) return 3;
+		if(dx[x] != 0) return dx[x];
+		return dx[x] = (dp(x-1) + 2*dp(x-2)) % 10007;
+	}
+
+}
