5b6209d5d82d0394180af517cd922663e7088d94
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-03-30 20:26:17 +0900

    #동전 #Silver1

---
+++ DP/Main_2293.java
@@ -0,0 +1,27 @@
+package ssafy_algo;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_2293 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n + 1];
+		int[] dp = new int[k + 1];
+		dp[0] = 1;
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			for (int j = arr[i]; j <= k; j++) {
+				dp[j] += dp[j - arr[i]];
+			}
+
+		}
+		System.out.println(dp[k]);
+	}
+
+}
