b2013ed2a331a5acb1e8758b81399750589fe5d8
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-06-02 09:40:32 +0900

    #색종이2 #Silver5

--- IM대비/Main_2567.java
+++ IM대비/Main_2567.java
@@ -1,40 +1,39 @@
-package Im_ex;
-
 import java.io.*;
 import java.util.*;
 
 public class Main_2567 {
+	static int[] dx = { -1, 0, 1, 0 }; // 상 우 하 좌
+	static int[] dy = { 0, 1, 0, -1 };
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[][] map = new int[101][101];
-		int[] dx = { -1, 0, 1, 0 }; // 상 우 하 좌
-		int[] dy = { 0, 1, 0, -1 };
 		int n = sc.nextInt();
-		for (int i = 0; i < n; i++) {
+		int[][] map = new int[101][101];
+		int cnt = 0;
+		for (int k = 0; k < n; k++) {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
 
-			for (int k = x; k < x + 10; k++) {
-				for (int r = y; r < y + 10; r++) {
-					map[k][r] = 1;
+			for (int i = x; i < x + 10; i++) {
+				for (int j = y; j < y + 10; j++) {
+					map[i][j] = 1;
 				}
 			}
+
 		}
-		int cnt = 0;
-		for (int i = 1; i <= 100; i++) {
-			for (int j = 1; j <= 100; j++) {
+
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
 				if (map[i][j] == 1) {
+					
+					for (int dir = 0; dir < 4; dir++) {
+						int nx = i + dx[dir];
+						int ny = j + dy[dir];
 
-					for (int k = 0; k < 4; k++) {
-						int nx = i + dx[k];
-						int ny = j + dy[k];
-
-						if (map[nx][ny] == 0) {
+						if (map[nx][ny] == 0)
 							cnt++;
-						}
-
 					}
+
 				}
 			}
 		}
