3da4181d2011f800b8fce8ef6663787b63c29ca8
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-07-14 21:02:57 +0900

    #그대로 출력하기 #Bronze3

---
+++ 구현/Main_11718.java
@@ -0,0 +1,17 @@
+package algo;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_11718 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = "";
+		while ((str = br.readLine()) != null) {
+			System.out.println(str);
+		}
+
+	}
+
+}
