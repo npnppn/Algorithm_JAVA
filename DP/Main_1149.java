f0a00805b08892dbc4694dd3b95d5bfd02edc06b
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-08-10 00:00:33 +0900

    #RGB 거리 #Silver1 #dp

--- DP/Main_1149.java
+++ DP/Main_1149.java
@@ -1,31 +1,32 @@
-package ssafy_algo;
+package dp;
+
 import java.io.*;
 import java.util.*;
 
 public class Main_1149 {
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
- 
-		int N = Integer.parseInt(br.readLine());
-		int[][] Cost = new int[N][3]; //비용 담을 배열
- 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			Cost[i][0] = Integer.parseInt(st.nextToken()); //빨강
-			Cost[i][1] = Integer.parseInt(st.nextToken()); //초록
-			Cost[i][2] = Integer.parseInt(st.nextToken()); //파랑
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] cost = new int[n][3]; // ���
+		int[][] dp = new int[1001][3]; // �ּҰ� �����Ϸ���
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 3; j++) {
+				cost[i][j] = sc.nextInt();
+			}
 		}
- 
-		// 1부터 N-1까지 각 i별 i-1의 서로 다른 색상 중 최솟값을 누적하여 더한다.  
-		for (int i = 1; i < N; i++) {
-			Cost[i][0] += Math.min(Cost[i - 1][1], Cost[i - 1][2]);
-			Cost[i][1] += Math.min(Cost[i - 1][0], Cost[i - 1][2]);
-			Cost[i][2] += Math.min(Cost[i - 1][0], Cost[i - 1][1]);
+		dp[0][0] = cost[0][0];
+		dp[0][1] = cost[0][1];
+		dp[0][2] = cost[0][2];
+
+		for (int i = 1; i < n; i++) {
+			dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+			dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+			dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][0]) + cost[i][2];
 		}
- 
-		System.out.println(Math.min(Math.min(Cost[N - 1][0], Cost[N - 1][1]), Cost[N - 1][2]));
+		System.out.println(Math.min(Math.min(dp[n-1][0], dp[n-1][1]), dp[n-1][2]));
+
 	}
- 
+
 }
