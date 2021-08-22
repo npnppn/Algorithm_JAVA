50ddb283c5db94bc0143eef29a0c325c7f510bef
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-08-22 23:47:16 +0900

    #경쟁적전염 #bfs #조금 어려웠음. #계속 풀어보자 bfs

--- DFS&BFS/Main_18405.java
+++ DFS&BFS/Main_18405.java
@@ -1,86 +1,71 @@
-package ssafy_algo;
+package dfs_bfs;
 
 import java.io.*;
 import java.util.*;
 
 public class Main_18405 {
+	static class Point {
+		int x, y, v;
 
-	static int N;
-
-	static int[][] map;
-	static int[] dx = { 1, -1, 0, 0 };
-	static int[] dy = { 0, 0, 1, -1 };
-
-	public static void main(String[] args) throws Exception, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-
-		map = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
+		public Point(int x, int y, int v) {
+			this.x = x;
+			this.y = y;
+			this.v = v;
 		}
-
-		st = new StringTokenizer(br.readLine());
-		int S = Integer.parseInt(st.nextToken());
-		int X = Integer.parseInt(st.nextToken())-1;
-		int Y = Integer.parseInt(st.nextToken())-1;
-
-		// 루프 처리안해주니 시간초과나옴.. x,y 위치 값이 0이 아니면 루프 탈출하도록 설정하면될듯?
-		loop: for (int i = 0; i < S; i++) {
-			for (int j = 0; j < K; j++) {
-				bfs(j + 1);
-
-				if (map[X][Y] != 0)
-					break loop;
-			}
-		}
-
-		System.out.println(map[X][Y]);
-
 	}
 
-	// 맵 돌아다니면서 전이할 바이러스 번호와 일치하면 큐에 다 넣음
-	static void bfs(int virus) {
-		Queue<Dot> q = new LinkedList<>();
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (map[i][j] == virus) {
-					q.add(new Dot(i, j));
+	static int n, k;
+	static int[] dx = { 1, 0, -1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
+	static int[][] map = new int[201][201];
+	static Queue<Point> q = new PriorityQueue<>((a, b) -> a.v - b.v);
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		k = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				map[i][j] = sc.nextInt();
+				
+				if (map[i][j] != 0) {
+					q.add(new Point(i, j, map[i][j]));
+
 				}
 			}
 		}
 
-		while (!q.isEmpty()) {
-			Dot qq = q.poll();
-			for (int k = 0; k < 4; k++) {
-				int nx = qq.x + dx[k];
-				int ny = qq.y + dy[k];
+		int s = sc.nextInt();
+		int endx = sc.nextInt();
+		int endy = sc.nextInt();
 
-				if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-					continue;
-				if (map[nx][ny] != 0)
-					continue;
-
-				map[nx][ny] = virus;
-
-			}
-
+		int t = 0;
+		while (t++ != s) {
+			bfs();
 		}
+		System.out.println(map[endx][endy]);
 
 	}
 
-	static class Dot {
-		int x, y;
+	static void bfs() {
+		Queue<Point> tmp = new PriorityQueue<>((a, b) -> a.v - b.v);
+		while (!q.isEmpty()) {
+			Point p = q.poll();
+			int x = p.x;
+			int y = p.y;
+			int v = p.v;
 
-		public Dot(int x, int y) {
-			this.x = x;
-			this.y = y;
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (ny < 1 || nx < 1 || ny > n || nx > n || map[nx][ny] != 0)
+					continue;
+				map[nx][ny] = v;
+				tmp.add(new Point(nx, ny, v));
+			}
+
 		}
+		q = tmp;
 
 	}
 
