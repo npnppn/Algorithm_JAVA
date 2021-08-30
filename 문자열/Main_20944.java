ce4201462bd7976cb52cb4b4a3f6dbd3ba83e15e
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-08-30 21:12:37 +0900

    Create Main_20944.java

---
+++ 문자열/Main_20944.java
@@ -0,0 +1,17 @@
+package string;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_20944 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		char[] ch = new char[n];
+		Arrays.fill(ch, 'a');
+		System.out.println(ch);
+
+	}
+
+}
