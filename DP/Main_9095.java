96b433f800b66633fb15bc32126597498f8f5d19
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-09-15 23:59:34 +0900

    #123 더하기 #Silver3 #dp 기본문제 #이젠 잘풀림

--- DP/Main_9095.java
+++ DP/Main_9095.java
@@ -1,23 +1,27 @@
-package baekjoon;
+package dp;
 
 import java.io.*;
 import java.util.*;
 
 public class Main_9095 {
+	static int[] dp = new int[11];
 
-	public static void main(String[] args) throws Exception, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] dp = new int[12];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+
 		dp[1] = 1; // 1
-		dp[2] = 2; // 1+1, 2
-		dp[3] = 4; // 1+1+1, 2+1, 1+2, 3
-		for (int i = 0; i < n; i++) {
-			int k = Integer.parseInt(br.readLine());
-			for (int j = 4; j <= 10; j++) {
-				dp[j] = dp[j - 3] + dp[j - 2] + dp[j - 1]; //1, 2, 3 세가지만 이용해서 문제를 풀어야함
+		dp[2] = 2; // 1+1 2
+		dp[3] = 4;// 1+1+1 1+2 2+1 3
+
+		for (int tc = 0; tc < t; tc++) {
+			int num = sc.nextInt();
+			for (int i = 4; i < 11; i++) {
+				dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+
 			}
-			System.out.println(dp[k]);
+			System.out.println(dp[num]);
+
 		}
 
 	}
