cf93bddf50b845f144612267ec980b4ee5c0e06d
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-09-17 02:39:08 +0900

    #두 수의 합 #Silver3 #투포인터 가장 기본문제

--- 투 포인터/Main_3273.java
+++ 투 포인터/Main_3273.java
@@ -1,37 +1,34 @@
-package baekjoon;
+package binary_search;
 
 import java.io.*;
 import java.util.*;
 
 public class Main_3273 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
 
-	public static void main(String[] args) throws Exception, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		int cnt = 0;
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
 		}
-
-		int x = Integer.parseInt(br.readLine());
 		Arrays.sort(arr);
+		int target = sc.nextInt();
 		int left = 0;
-		int right = n - 1;
+		int right = N - 1;
+		int cnt = 0;
 		while (left < right) {
-			if (arr[left] + arr[right] >= x) {
-				if (arr[left] + arr[right] == x)
+			if (arr[left] + arr[right] >= target) {
+				if (arr[left] + arr[right] == target) {
 					cnt++;
+				}
+				//합한게 더 타겟보다 크면 최대값를 줄이고
 				right--;
-
-			} else {
+			} else { //타켓이 더 큰거면 최솟값을 늘려
 				left++;
 			}
 		}
-
 		System.out.println(cnt);
 
 	}
-
 }
