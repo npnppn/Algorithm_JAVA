2827bdeed87691e4c0d5b930b5547ec05e81448a
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2021-12-28 14:53:23 +0900

    #직각삼각형 #Bronze3 #풀이 완료

---
+++ BOJ_솔브닥 Class/Class 2/Main_4153.java
@@ -0,0 +1,32 @@
+package class2;
+
+import java.io.*;
+import java.util.*;
+
+//숫자 정렬해서 피타고라스 사용!
+public class Main_4153 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			int[] arr = new int[3];
+			for (int i = 0; i < 3; i++) {
+				arr[i] = sc.nextInt();
+			}
+
+			if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0)
+				break;
+
+			Arrays.sort(arr);
+
+			if (arr[0] * arr[0] + arr[1] * arr[1] == arr[2] * arr[2]) {
+				System.out.println("right");
+			} else {
+				System.out.println("wrong");
+			}
+
+		}
+
+	}
+
+}
